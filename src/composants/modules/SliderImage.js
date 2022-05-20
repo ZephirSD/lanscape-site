45f8b4c55a1c6aaef5096fb8f33afe7d307b7826
ZephirSD <siegfrieddallery@gmail.com>
2022-05-20 14:37:35 +0400

    Création de la base de données des images

---
+++ src/composants/modules/SliderImage.js
@@ -0,0 +1,22 @@
+import image1 from "../../assets/image1.jpg";
+import image2 from "../../assets/image2.jpg";
+import image3 from "../../assets/image3.jpg";
+
+export const sliderImage = [
+    {
+        id: 1,
+        url_image: image1
+    },
+    {
+        id: 2,
+        url_image: image2
+    },
+    {
+        id: 3,
+        url_image: image3
+    },
+    {
+        id: 4,
+        url_image: image2
+    }
+]
