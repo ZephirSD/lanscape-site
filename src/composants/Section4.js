824b92b1703b8c7bff0f3d336b87c84249e45ce0
ZephirSD <siegfrieddallery@gmail.com>
2022-05-20 14:38:41 +0400

    Création de la section de Contact

---
+++ src/composants/Section4.js
@@ -0,0 +1,11 @@
+function Section4() {
+    return ( 
+        <>
+            <section className="section4">
+                <h1 className="titre-about">Contact</h1>
+            </section>
+        </> 
+    );
+}
+
+export default Section4;
