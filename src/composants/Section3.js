5be7eec326add3352abe8af747378724883e9af4
ZephirSD <siegfrieddallery@gmail.com>
2022-05-20 14:38:25 +0400

    Insertion du package `Swiper JS` pour le style et des composants

--- src/composants/Section3.js
+++ src/composants/Section3.js
@@ -1,8 +1,33 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay, EffectFade } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import "swiper/css/effect-fade";
+import { sliderImage } from './modules/SliderImage';
 function Section3() {
     return ( 
         <>
             <section className="section3">
                 <h1 className="titre-about">Services</h1>
+                <div className='slider-box'>
+                <Swiper
+                    pagination={true} 
+                    modules={[Pagination,Autoplay,EffectFade]}
+                    slidesPerView={1}
+                    effect={'fade'}
+                    loop={true}
+                    autoplay={{
+                        delay: 3500,
+                        disableOnInteraction: true,
+                    }}
+                    >
+                    {sliderImage.map((slideImage) => (
+                        <SwiperSlide key={slideImage.id} style={{backgroundImage: `url(${slideImage.url_image})`}}/>
+                    ))}
+                    ...
+                </Swiper>
+                </div>
             </section>
         </> 
     );
