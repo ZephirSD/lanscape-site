b0cee34714cfcebcc9f34964f6d339f4a0412b67
ZephirSD <siegfrieddallery@gmail.com>
2022-05-21 09:46:43 +0400

    Insertion des composants de formulaires

--- src/composants/Section4.js
+++ src/composants/Section4.js
@@ -1,8 +1,18 @@
+import { Button, Input, Textarea } from "./modules/Forms";
+
 function Section4() {
     return ( 
         <>
             <section className="section4">
-                <h1 className="titre-about">Contact</h1>
+                <h1 className="titre-about" data-aos="fade-right">Contact</h1>
+                <div className="block-contact">
+                    <form>
+                        <Input type={"email"} label={"Email"}/>
+                        <Textarea rows={"10"} label={"Message"} maxLength={"100"}/>
+                        <Input type={"checkbox"} label={"J'accepte les conditions de confidentialté aux normes RGPD"}/>
+                        <Button type={"button"} value={"Envoyer"}/>
+                    </form>
+                </div>
             </section>
         </> 
     );
