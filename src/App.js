b1cc83922f25fce32c1449e3b63549debad22f20
ZephirSD <siegfrieddallery@gmail.com>
2022-05-23 09:21:34 +0400

    Nettoyage du composant `App.js`

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
-// import Scrollbar from 'smooth-scrollbar';
 import { useEffect } from "react";
 import Section1 from "./composants/Section1";
 import Section2 from "./composants/Section2";
 import Section3 from "./composants/Section3";
 import './style/style.scss';
 import Section4 from "./composants/Section4";
+
 function App() {
   return (
     useEffect(() => {
       AOS.init();
       AOS.refresh();
-      // Scrollbar.initAll();
     }),
     (
       <>
